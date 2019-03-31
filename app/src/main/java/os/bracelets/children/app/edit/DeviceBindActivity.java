5cb18de0fa9ffb7fd3c7151f143a86df3dce24af
lishiyou <sbandc351820>
2019-04-01 01:23:22 +0800

    新增功能

--- app/src/main/java/os/bracelets/children/app/edit/DeviceBindActivity.java
+++ app/src/main/java/os/bracelets/children/app/edit/DeviceBindActivity.java
@@ -1,12 +1,16 @@
 package os.bracelets.children.app.edit;
 
+import android.content.Intent;
+import android.media.Image;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import aio.health2world.http.HttpResult;
+import aio.health2world.qrcode.CaptureActivity;
 import aio.health2world.utils.ToastUtil;
 import aio.health2world.view.LoadingDialog;
 import os.bracelets.children.AppConfig;
@@ -24,7 +28,7 @@ import os.bracelets.children.view.TitleBar;
 public class DeviceBindActivity extends BaseActivity {
 
     private TitleBar titleBar;
-
+    private ImageView ivScan;
     private EditText edDeviceNo;
     private TextView tvName;
     private Button btnBind;
@@ -40,6 +44,7 @@ public class DeviceBindActivity extends BaseActivity {
     @Override
     protected void initView() {
         titleBar = findView(R.id.titleBar);
+        ivScan = findView(R.id.ivScan);
         TitleBarUtil.setAttr(this, "", "绑定设备", titleBar);
 
         edDeviceNo = findView(R.id.edDeviceNo);
@@ -56,6 +61,7 @@ public class DeviceBindActivity extends BaseActivity {
 
     @Override
     protected void initListener() {
+        ivScan.setOnClickListener(this);
         btnBind.setOnClickListener(this);
         titleBar.setLeftClickListener(new View.OnClickListener() {
             @Override
@@ -67,38 +73,57 @@ public class DeviceBindActivity extends BaseActivity {
 
     @Override
     public void onClick(View v) {
+        if (v.getId() == R.id.ivScan) {
+            Intent intent = new Intent(this, CaptureActivity.class);
+            startActivityForResult(intent, 0x11);
+        }
         if (v.getId() == R.id.btnBind) {
             String deviceNo = edDeviceNo.getText().toString().trim();
             if (TextUtils.isEmpty(deviceNo)) {
                 ToastUtil.showShort("请输入设备编号");
                 return;
             }
-
-            ApiRequest.bindDevice(String.valueOf(member.getAccountId()), deviceNo,
-                    new HttpSubscriber() {
-                        @Override
-                        public void onStart() {
-                            super.onStart();
-                            dialog.show();
-                        }
-
-                        @Override
-                        public void onError(Throwable e) {
-                            super.onError(e);
-                            dialog.dismiss();
-                        }
-
-                        @Override
-                        public void onNext(HttpResult result) {
-                            super.onNext(result);
-                            dialog.dismiss();
-                            if (result.code.equals(AppConfig.SUCCESS)) {
-                                ToastUtil.showShort("操作成功");
-                                finish();
-                            }
-                        }
-                    });
+            bindDevice(deviceNo);
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK)
+            return;
+        if (requestCode == 0x11) {
+            String result = data.getStringExtra("result");
+            if (!TextUtils.isEmpty(result))
+                edDeviceNo.setText(result);
+        }
+    }
+
+    private void bindDevice(String deviceNo) {
+        ApiRequest.bindDevice(String.valueOf(member.getAccountId()), deviceNo,
+                new HttpSubscriber() {
+                    @Override
+                    public void onStart() {
+                        super.onStart();
+                        dialog.show();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        super.onError(e);
+                        dialog.dismiss();
+                    }
+
+                    @Override
+                    public void onNext(HttpResult result) {
+                        super.onNext(result);
+                        dialog.dismiss();
+                        if (result.code.equals(AppConfig.SUCCESS)) {
+                            ToastUtil.showShort("操作成功");
+                            finish();
+                        }
+                    }
+                });
     }
 
     @Override
---
+++ app/src/main/java/os/bracelets/children/app/edit/LabelAddActivity.java
@@ -0,0 +1,26 @@
+package os.bracelets.children.app.edit;
+
+import os.bracelets.children.common.BaseActivity;
+
+public class LabelAddActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+}
