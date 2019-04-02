4adb76ad239815ea6f6eee983465b8838b7a0012
lishiyou <sbandc351820>
2019-04-03 00:46:46 +0800

    新增功能

--- app/src/main/java/os/bracelets/children/app/edit/EditRemindActivity.java
+++ app/src/main/java/os/bracelets/children/app/edit/EditRemindActivity.java
@@ -1,14 +1,22 @@
 package os.bracelets.children.app.edit;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import aio.health2world.http.HttpResult;
+import aio.health2world.pickeview.TimePickerView;
 import aio.health2world.utils.Logger;
+import aio.health2world.utils.TimePickerUtil;
 import aio.health2world.utils.ToastUtil;
 import os.bracelets.children.AppConfig;
 import os.bracelets.children.R;
@@ -19,7 +27,7 @@ import os.bracelets.children.http.HttpSubscriber;
 import os.bracelets.children.utils.TitleBarUtil;
 import os.bracelets.children.view.TitleBar;
 
-public class EditRemindActivity extends BaseActivity {
+public class EditRemindActivity extends BaseActivity implements TimePickerView.OnTimeSelectListener {
 
     private TitleBar titleBar;
     private FamilyMember member;
@@ -27,6 +35,13 @@ public class EditRemindActivity extends BaseActivity {
     private TextView tvCycle, tvTime;
     private Button btnSave;
 
+    private SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+    private TimePickerView pickerView;
+    private String[] date = {"周一", "周二", "周三", "周四", "周五", "周六", "周日"};
+    private boolean[] checked = {false, false, false, false, false, false, false};
+    private String remindPeriod = "";
+    private String remind = "";
+    private AlertDialog alertDialog;
     private LinearLayout llRemindTime, llRemindCycle;
 
     @Override
@@ -50,7 +65,7 @@ public class EditRemindActivity extends BaseActivity {
     @Override
     protected void initData() {
         member = (FamilyMember) getIntent().getSerializableExtra("member");
-        Logger.i("member", member.toString());
+        pickerView = TimePickerUtil.initTime(this, this);
     }
 
     @Override
@@ -73,10 +88,54 @@ public class EditRemindActivity extends BaseActivity {
                 remind();
                 break;
             case R.id.llRemindCycle:
+                if (alertDialog == null)
+                    alertDialog = new AlertDialog.Builder(this)
+                            .setTitle("选择周期")
+                            .setMultiChoiceItems(date, checked, new DialogInterface.OnMultiChoiceClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+
+                                }
+                            })
+                            .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    remindPeriod = "";
+                                    remind = "";
+                                    for (int i = 0; i < checked.length; i++) {
+                                        if (checked[i])
+                                            remindPeriod += (i + 1) + ";";
+                                        if (i == 0 && checked[i])
+                                            remind += "周一" + " ";
+                                        if (i == 1 && checked[i])
+                                            remind += "周二" + " ";
+                                        if (i == 2 && checked[i])
+                                            remind += "周三" + " ";
+                                        if (i == 3 && checked[i])
+                                            remind += "周四" + " ";
+                                        if (i == 4 && checked[i])
+                                            remind += "周五" + " ";
+                                        if (i == 5 && checked[i])
+                                            remind += "周六" + " ";
+                                        if (i == 6 && checked[i])
+                                            remind += "周日";
+                                    }
+                                    tvCycle.setText(remind);
+                                }
+                            })
+                            .create();
+                alertDialog.show();
                 break;
             case R.id.llRemindTime:
+                pickerView.show();
                 break;
         }
+    }
+
+    @Override
+    public void onTimeSelect(Date date, View v) {
+        String time = format.format(date);
+        tvTime.setText(time);
     }
 
     private void remind() {
@@ -93,18 +152,17 @@ public class EditRemindActivity extends BaseActivity {
             ToastUtil.showShort("内容不能为空");
             return;
         }
-//        if (TextUtils.isEmpty(cycle)) {
-//            ToastUtil.showShort("请设置提醒周期");
-//            return;
-//        }
-//
-//        if (TextUtils.isEmpty(time)) {
-//            ToastUtil.showShort("请设置提醒时间");
-//            return;
-//        }
-
-        ApiRequest.remind(String.valueOf(member.getAccountId()), title, content, "1;2;3;4;5;6;7",
-                "00-30-00", new HttpSubscriber() {
+        if (TextUtils.isEmpty(cycle)) {
+            ToastUtil.showShort("请设置提醒周期");
+            return;
+        }
+        if (TextUtils.isEmpty(time)) {
+            ToastUtil.showShort("请设置提醒时间");
+            return;
+        }
+        remindPeriod = remindPeriod.substring(0, remindPeriod.length() - 1);
+        ApiRequest.remind(String.valueOf(member.getAccountId()), title, content, remindPeriod, time,
+                new HttpSubscriber() {
                     @Override
                     public void onError(Throwable e) {
                         super.onError(e);
