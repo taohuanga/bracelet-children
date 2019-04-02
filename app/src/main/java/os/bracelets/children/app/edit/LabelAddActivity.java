a6ffa7096fdc23de3ea3bede3bde4f7c76d45b68
lishiyou <lishiyou>
2019-04-02 09:51:22 +0800

    bug fix

--- app/src/main/java/os/bracelets/children/app/edit/LabelAddActivity.java
+++ app/src/main/java/os/bracelets/children/app/edit/LabelAddActivity.java
@@ -1,26 +1,26 @@
-package os.bracelets.children.app.edit;
-
-import os.bracelets.children.common.BaseActivity;
-
-public class LabelAddActivity extends BaseActivity {
-
-    @Override
-    protected int getLayoutId() {
-        return 0;
-    }
-
-    @Override
-    protected void initView() {
-
-    }
-
-    @Override
-    protected void initData() {
-
-    }
-
-    @Override
-    protected void initListener() {
-
-    }
-}
+//package os.bracelets.children.app.edit;
+//
+//import os.bracelets.children.common.BaseActivity;
+//
+//public class LabelAddActivity extends BaseActivity {
+//
+//    @Override
+//    protected int getLayoutId() {
+//        return 0;
+//    }
+//
+//    @Override
+//    protected void initView() {
+//
+//    }
+//
+//    @Override
+//    protected void initData() {
+//
+//    }
+//
+//    @Override
+//    protected void initListener() {
+//
+//    }
+//}
