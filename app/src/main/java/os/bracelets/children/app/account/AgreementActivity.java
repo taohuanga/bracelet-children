109ad70d2a6a81ec6b8d782a3a28ba7bf0d43210
lishiyou <sbandc351820>
2020-01-14 22:24:04 +0800

    用户协议接口获取

--- app/src/main/java/os/bracelets/children/app/account/AgreementActivity.java
+++ app/src/main/java/os/bracelets/children/app/account/AgreementActivity.java
@@ -1,14 +1,30 @@
 package os.bracelets.children.app.account;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.http.SslError;
 import android.view.View;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import aio.health2world.http.HttpResult;
+import os.bracelets.children.AppConfig;
 import os.bracelets.children.R;
 import os.bracelets.children.common.BaseActivity;
+import os.bracelets.children.http.ApiRequest;
+import os.bracelets.children.http.HttpSubscriber;
 import os.bracelets.children.utils.TitleBarUtil;
 import os.bracelets.children.view.MyWebView;
 import os.bracelets.children.view.TitleBar;
@@ -23,6 +39,8 @@ public class AgreementActivity extends BaseActivity {
 
     private Button btnDisAgree,btnAgree;
 
+    private ProgressBar progress;
+
     @Override
     protected int getLayoutId() {
         return R.layout.activity_agreement;
@@ -34,6 +52,7 @@ public class AgreementActivity extends BaseActivity {
         btnAgree = findView(R.id.btnAgree);
         btnDisAgree = findView(R.id.btnDisAgree);
         layoutBottom = findView(R.id.layoutBottom);
+        progress = findView(R.id.progress);
         TitleBarUtil.setAttr(this, "", getString(R.string.user_agreement), titleBar);
 
         webView = findView(R.id.webView);
@@ -48,8 +67,31 @@ public class AgreementActivity extends BaseActivity {
         settings.setSupportZoom(true);
         settings.setUseWideViewPort(true);
         settings.setLoadWithOverviewMode(true);
-        webView.loadUrl("http://wx.zgznhh.com/Agreement.html");
+//        webView.loadUrl("http://wx.zgznhh.com/Agreement.html");
 //        webView.loadUrl("file:///android_asset/html/Agreement.html");
+
+        ApiRequest.helpUrl(new HttpSubscriber() {
+            @Override
+            public void onNext(HttpResult result) {
+                super.onNext(result);
+                if (result.code.equals(AppConfig.SUCCESS)) {
+                    try {
+                        JSONObject object = new JSONObject(new Gson().toJson(result.data));
+                        final String agreementUrl = object.optString("agreementUrl");
+                        webView.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                layoutBottom.setVisibility(View.VISIBLE);
+                                webView.loadUrl(agreementUrl);
+                            }
+                        },500);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
     }
 
     @Override
@@ -77,19 +119,38 @@ public class AgreementActivity extends BaseActivity {
             }
         });
 
-//        webView.setOnCustomScrollChangeListener(new MyWebView.ScrollInterface() {
-//            @Override
-//            public void onSChanged(int l, int t, int oldl, int oldt) {
-//                //WebView的总高度
-//                float webViewContentHeight = webView.getContentHeight() * webView.getScale();
-//                //WebView的现高度
-//                float webViewCurrentHeight = (webView.getHeight() + webView.getScrollY());
-//                if ((webViewContentHeight - webViewCurrentHeight) <=20) {
-//                    layoutBottom.setVisibility(View.VISIBLE);
-//                } else {
-//                    layoutBottom.setVisibility(View.GONE);
-//                }
-//            }
-//        });
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                progress.setProgress(newProgress);
+            }
+        });
+
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progress.setVisibility(View.VISIBLE);
+            }
+
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progress.setVisibility(View.GONE);
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return false;
+            }
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+        });
     }
 }
